79cc5367c0863738b04e9ac10c01d7a19748b4b8
Васильев Александр <wyndace.av@ya.ru>
2023-11-11 14:04:32 +0300

    feat: add poetry files

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
+from src.application.database.models.unit import Unit
+from src.application.database.utils.session import DBSession
 from src.application.utils import menu
-from src.application import controllers
+from src.application.controllers import parserconsole, parserdatabase, databasetg
 
 
 def main():
     items = (
-        "Показать заголовок страницы example.com",
+        "Показать ссылки на тесты в этом месяце",
+        "Показать тесты в этом месяце",
+        "Сохранить тесты этого месяца в Б/Д",
     )
     choice = menu.multi_chooser_menu("Выберите пункт меню: ", items)
     if choice == 1:
-        controllers.parserconsole()
+        parserconsole.print_dates_link_to_console()
+    if choice == 2:
+        parserconsole.print_unit_to_console()
+    if choice == 3:
+        parserdatabase.save_unit_to_db()
+    DBSession().close_session()
 
 
 if __name__ == "__main__":

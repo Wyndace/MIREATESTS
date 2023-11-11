b5e8f16e8b2891ecd523e8f5e2b95a8ee4902c52
Васильев Александр <wyndace.av@ya.ru>
2023-11-11 14:00:56 +0300

    feat: add printer units to console controller

--- src/application/controllers/parserconsole.py
+++ src/application/controllers/parserconsole.py
@@ -1,12 +1,23 @@
 from httpx import Client
+
 from ..web import parser
-from ...config import urls
+from ...config import constants
 from icecream import ic
 
 
-def print_heading_to_console():
+def print_dates_link_to_console():
     client = Client()
-    raw_html = parser.get_html(client, urls.HTTPS_BASE)
-    if raw_html:
-        heading = parser.get_heading(raw_html)
-        ic(heading)
+    login_token = parser.get_logintoken(client)
+    parser.login(client, constants.MIREA_LOGIN,
+                 constants.MIREA_PASSWORD, login_token)
+    ic(parser.get_tests_url(client))
+
+
+def print_unit_to_console():
+    client = Client()
+    login_token = parser.get_logintoken(client)
+    parser.login(client, constants.MIREA_LOGIN,
+                 constants.MIREA_PASSWORD, login_token)
+    links = parser.get_tests_url(client)
+    for link in links:
+        ic(parser.get_unit(client, link))

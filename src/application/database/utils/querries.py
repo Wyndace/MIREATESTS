0a9b9649de55eba76cd47fa7d2e0a2e6475edac2
Васильев Александр <wyndace.av@ya.ru>
2023-11-11 13:43:48 +0300

    refactor: change protected to public visibility

--- src/application/database/utils/querries.py
+++ src/application/database/utils/querries.py
@@ -1,6 +1,8 @@
 from json import loads
+
+from icecream import ic
 from ..models.base import BaseModel
-from typing import Type
+from typing import Any, Type
 from .session import DBSession
 
 
@@ -40,20 +42,21 @@ def clear_models(session: DBSession, model: Type[BaseModel]):
         session.delete_model(item)
 
 
-def _save_dict_to_db(session: DBSession,
-                     model: Type[BaseModel], dict_data: dict[str, dict]):
-    clear_models(session, model)
-    model_fields = list(model.__dict__.keys())
-    model_fields.remove("id")
-    for value in dict_data.values():
-        filtered_value = {key: value[key]
-                          for key in value if key in model_fields}
-        item = model(**filtered_value)
-        session.add_model(item)
+def save_dict_to_db(session: DBSession,
+                    model: Type[BaseModel], dict_data: dict[str, Any]):
+    # clear_models(session, model)
+    # model_fields = list(model.__dict__.keys())
+    # model_fields.remove("id")
+    # for value in dict_data.values():
+    #     ic(value)
+    #     filtered_value = {key: value[key]
+    #                       for key in value if key in model_fields}
+    item = model(**dict_data)
+    session.add_model(item)
     session.commit_session()
 
 
 def save_json_to_db(session: DBSession,
                     model: Type[BaseModel], json_data: str):
     dict_data = loads(json_data)
-    _save_dict_to_db(session, model, dict_data)
+    save_dict_to_db(session, model, dict_data)
